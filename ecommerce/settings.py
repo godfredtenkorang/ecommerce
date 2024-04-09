9866a176ec74f6280070d8867cf2564a3d4431b7
godeytech <godfredtenkorang9@gmail.com>
2024-04-09 21:09:12 +0000

    added static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.endlessessencials.com','167.99.54.186', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.endlessessencials.com', '167.99.54.186', 'localhost', '127.0.0.1']
 
 
 # Application definition
