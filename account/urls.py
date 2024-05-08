c757d22183d4937743177874e4736ab2f15817fd
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-05-08 15:29:19 +0000

    changes

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('register', views.register, name='register'),
+    path('register/', views.register, name='register'),
 
     # Email verification URL's
 
