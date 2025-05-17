95ffb12172bf1d1362dbd8270891c454aab13191
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-05-17 10:43:05 +0000

    changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.endlessessencials.com','157.245.223.195', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['.godfred10.pythonanywhere.com', 'localhost','127.0.0.1']
 
 
 # Application definition
